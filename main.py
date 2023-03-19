e83ca23d99a72211e5ba6145e2d6958fea0ed2cf
David Thomson <31463870+MegaBoy117@users.noreply.github.com>
2023-03-19 16:54:04 +0000

    Started implementation of averaging the algorithm over each shuffle_amount

--- main.py
+++ main.py
@@ -17,31 +17,33 @@ PARSIMONY_SIZE = 1.2
 LAMBDA = 2000
 
 if __name__ == "__main__":
-
+    average_amount = 5
+    cubes = {}
     for shuffle_amount in range(1, 20):
-        start_time = time.time()
-        cube = cube.Cube(3)
-        shuffle_moves = cube.random_moves(7)
-        cube.run_moves(shuffle_moves)
+        for i in range(average_amount):
+            start_time = time.time()
+            cube = cube.Cube(3)
+            shuffle_moves = cube.random_moves(7)
+            cube.run_moves(shuffle_moves)
 
-        dkho = DKHO.DKHO(cube, NUM_KRILL, MAX_GENERATIONS, CXPB, MUTPB, EVAL_DEPTH, MIN_MUTATE, MAX_MUTATE, SELECTION_SIZE, PARSIMONY_SIZE, LAMBDA)
-        hof = dkho.get_hof()
-        solve_moves = []
+            dkho = DKHO.DKHO(cube, NUM_KRILL, MAX_GENERATIONS, CXPB, MUTPB, EVAL_DEPTH, MIN_MUTATE, MAX_MUTATE, SELECTION_SIZE, PARSIMONY_SIZE, LAMBDA)
+            hof = dkho.get_hof()
+            solve_moves = []
 
-        for solution in hof:
-            test_cube = copy.deepcopy(cube)
-            test_cube.run_moves(solution)
-            if test_cube.is_solved():
-                solve_moves = solution
-                break
+            for solution in hof:
+                test_cube = copy.deepcopy(cube)
+                test_cube.run_moves(solution)
+                if test_cube.is_solved():
+                    solve_moves = solution
+                    break
 
-        if not solve_moves:
-            print("No solution found")
-        cube.run_moves(solve_moves)
+            if not solve_moves:
+                print("No solution found")
+            cube.run_moves(solve_moves)
 
-        print("--- %s seconds ---" % (time.time() - start_time))
+            print("--- %s seconds ---" % (time.time() - start_time))
 
-    visualise.show_moves(shuffle_moves=shuffle_moves, solve_moves=solve_moves)
+   #visualise.show_moves(shuffle_moves=shuffle_moves, solve_moves=solve_moves)
 
 
 def plot_logbook(logbook):
